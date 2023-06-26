e4fdacf0e1ea6caae5a8c0040adaca87a99ac49d
Shivam <shivamguptanitw@gmail.com>
2023-06-26 17:41:37 +0530

    Commit 2: Login page UI designed and created.

--- pages/Register.js
+++ pages/Register.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Register = () => {
   return (
     <div>
-      Home
+      Register
     </div>
   )
 }
 
-export default Home
+export default Register
